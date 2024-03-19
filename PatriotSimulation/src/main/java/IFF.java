8d21030df8e956f76a12b3e6ae1adcbede896966
V Woerdings <Vijawoe@gmail.com>
2024-03-19 16:50:51 +0100

    Added documentation

--- PatriotSimulation/src/main/java/IFF.java
+++ PatriotSimulation/src/main/java/IFF.java
@@ -2,10 +2,24 @@ package main.java;
 
 public class IFF {
 
+	/**
+	 * {@code validate} defaults to <code>false</code>
+	 * 
+	 * @see IFF#parseRadarString(String, boolean)
+	 */
 	public static String[] parseRadarString(String radarString) throws IllegalArgumentException {
 		return parseRadarString(radarString, false);
 	}
 
+	/**
+	 * Splits one line of radar output into an array of binary numbers
+	 * 
+	 * @param radarString one line of radar output, separated by a semicolon
+	 * @param validate    if {@code true}, checks if {@code radarString} contains
+	 *                    unexpected values
+	 * @return array of binary numbers
+	 * @throws IllegalArgumentException if radar output contains unexpected values
+	 */
 	public static String[] parseRadarString(String radarString, boolean validate) throws IllegalArgumentException {
 		String[] radarData = radarString.split(";");
 		if (!validate || isBinary(radarData)) {
@@ -14,6 +28,13 @@ public class IFF {
 		throw new IllegalArgumentException("Invalid radar output");
 	}
 
+	/**
+	 * Detects foe, based on processed radar output
+	 * 
+	 * @param radarData array of binary numbers
+	 * @return {@code true} if {@code radarData} contains more uneven numbers than
+	 *         even numbers; {@code false} otherwise
+	 */
 	public static boolean detectFoe(String[] radarData) {
 		int nNums = radarData.length;
 		int nOdd = 0;
@@ -25,6 +46,11 @@ public class IFF {
 		return nOdd > nNums / 2;
 	}
 
+	/**
+	 * Checks if each string in the input array is binary
+	 * 
+	 * @see #isBinary(String)
+	 */
 	public static boolean isBinary(String[] input) {
 		for (String s : input) {
 			if (!isBinary(s)) {
@@ -34,6 +60,13 @@ public class IFF {
 		return true;
 	}
 
+	/**
+	 * Checks if input string is binary
+	 * 
+	 * @param input string to check
+	 * @return {@code true} if {@code input} only contains 0s and 1s; {@code false}
+	 *         otherwise
+	 */
 	public static boolean isBinary(String input) {
 		return input.replaceAll("0|1", "").length() == 0;
 	}
--- PatriotSimulation/src/main/java/MissileLauncher.java
+++ PatriotSimulation/src/main/java/MissileLauncher.java
@@ -4,17 +4,34 @@ import java.util.Random;
 
 public class MissileLauncher {
 	private final double PK;
+	private final static double DEFAULT_PK = 0.8;
 	Random r;
 
+	/**
+	 * {@code PK} defaults to {@value #DEFAULT_PK}
+	 * 
+	 * @see MissileLauncher#MissileLauncher(double)
+	 */
 	public MissileLauncher() {
-		this(0.8);
+		this(DEFAULT_PK);
 	}
 
+	/**
+	 * Creates MissileLauncher with specific probability of kill
+	 * 
+	 * @param PK probability of kill ratio, probability of a missile hitting its
+	 *           target
+	 */
 	public MissileLauncher(double PK) {
 		this.PK = PK;
 		r = new Random();
 	}
 
+	/**
+	 * Launches missile and checks if it hits its target
+	 * 
+	 * @return {@code true} if the target was hit; {@code false} otherwise
+	 */
 	public boolean launchMissile() {
 		System.out.println("Launching missile...");
 		if (r.nextDouble() > this.PK) {
